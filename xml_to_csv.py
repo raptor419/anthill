bd312369a574315e96a8c970f6a7d5370c831091
navstik <rakeshv_24@bitbucket.org>
2018-07-26 18:36:25 +0530

    x

---
+++ xml_to_csv.py
@@ -0,0 +1,36 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        #print(xml_file)
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size').find('width').text),
+                     int(root.find('size').find('height').text),
+                     member[0].text,
+                     int(member[4].find('xmin').text),
+                     int(member[4].find('ymin').text),
+                     int(member[4].find('xmax').text),
+                     int(member[4].find('ymax').text))
+            xml_list.append(value)
+            #print(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+
+def main():
+    for directory in ['train','test']:
+        image_path = os.path.join(os.getcwd(), 'images/{}'.format(directory))
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv('data/{}_labels.csv'.format(directory), index=None)
+        print('Successfully converted xml to csv.')
+
+main()
